a23df0b047b2e42a01ef7c9c52b524f873fe472d
Kavish <kavishrmunjal@gmail.com>
2025-08-25 18:37:39 -0500

    Server URL

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'weekend-tasks-v1';
 const urlsToCache = ['/', '/index.html', '/icon.png'];
 
-const version = '1.2.0';
+const version = '1.2.1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
