ec7e7dcbe206316eaa997d5a7ba817330ce12950
Kavish <kavishrmunjal@gmail.com>
2025-08-30 19:29:37 -0500

    Added Notifications

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'weekend-tasks-v1';
 const urlsToCache = ['/', '/index.html', '/icon.png'];
 
-const version = '1.2.1';
+const version = '1.2.2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
