9e0dcec8990bc65e60c85d62c7ac72ce13b9c739
Kavish <kavishrmunjal@gmail.com>
2025-09-07 12:31:03 -0500

    Force Update push notif

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 const CACHE_NAME = 'weekend-tasks-v1';
 const urlsToCache = ['/', '/index.html', '/icon.png'];
 
-const version = '1.2.10';
+const version = '1.2.11';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
